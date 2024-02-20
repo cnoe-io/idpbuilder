30f1d9db241fe74267b00cea587b5e855cb82660
Carlos Santana <csantana23@gmail.com>
2024-02-20 13:47:05 -0500

    setup port 8443
    Signed-off-by: Carlos Santana <csantana23@gmail.com>

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -13,3 +13,9 @@ sudo chown -R vscode:vscode /home/vscode/go
 
 # Add idpbuilder to path
 echo "export PATH=\$PATH:/home/vscode/go/src/github.com/cnoe-io/idpbuilder" >> $HOME/.bashrc
+
+# setup autocomplete for kubectl and alias k
+echo "source <(kubectl completion bash)" >> $HOME/.bashrc
+echo "alias k=kubectl" >> $HOME/.bashrc
+echo "complete -F __start_kubectl k" >> $HOME/.bashrc
+
