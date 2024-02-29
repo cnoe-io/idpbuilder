c682b5da3dd6f21bf374631e6283b92d20941f1a
Carlos Santana <csantana23@gmail.com>
2024-02-29 03:16:19 +0000

    remove extras
    Signed-off-by: Carlos Santana <csantana23@gmail.com>

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -5,7 +5,6 @@ curl -sLS https://get.arkade.dev | sudo sh
 arkade get kind
 arkade get kubectl
 arkade get helm
-arkade get gh
 echo "export PATH=\$PATH:/home/vscode/.arkade/bin" >> $HOME/.bashrc
 
 # Make sure go path is owned by vscode
@@ -15,6 +14,7 @@ sudo chown -R vscode:vscode /home/vscode/go
 echo "export PATH=\$PATH:/home/vscode/go/src/github.com/cnoe-io/idpbuilder" >> $HOME/.bashrc
 
 # setup autocomplete for kubectl and alias k
+mkdir $HOME/.kube
 echo "source <(kubectl completion bash)" >> $HOME/.bashrc
 echo "alias k=kubectl" >> $HOME/.bashrc
 echo "complete -F __start_kubectl k" >> $HOME/.bashrc
