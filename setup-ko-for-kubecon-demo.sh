33638da454e4d87c15d9f5867e6c04a0c023738a
Jesse Sanford <108698+jessesanford@users.noreply.github.com>
2024-03-19 19:35:00 -0400

    add ko setup script
    Signed-off-by: Jesse Sanford <108698+jessesanford@users.noreply.github.com>

---
+++ setup-ko-for-kubecon-demo.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+docker pull cgr.dev/chainguard/static:latest
+
+docker tag cgr.dev/chainguard/static:latest ko.local/chainguard/static:latest
+
+"defaultBaseImage: ko.local/chainguard/static" > .ko.yaml
+
+export KO_DOCKER_REPO=gitea.cnoe.localtest.me:8443/giteaadmin/ && kustomize build config/overlays/local | ko resolve --local --insecure-registry -f - | kubectl apply -f -
