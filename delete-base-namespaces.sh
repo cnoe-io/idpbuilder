2d4913687fde625ddda649e5d55c11189bdbee58
Jesse Sanford <108698+jessesanford@users.noreply.github.com>
2024-03-19 08:24:28 -0400

    wip
    Signed-off-by: Jesse Sanford <108698+jessesanford@users.noreply.github.com>

---
+++ delete-base-namespaces.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+kubectl delete namespace argocd
+kubectl delete namespace gitea
+kubectl delete namespace ingress-nginx
+kubectl delete namespace nginx
+kubectl delete namespace idpbuilder-localdev
