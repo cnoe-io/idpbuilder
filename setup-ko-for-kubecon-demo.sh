0a787ffbb46ac61b756f9053cacad23c00a21d84
Jesse Sanford <108698+jessesanford@users.noreply.github.com>
2024-03-19 19:41:33 -0400

    fix ko setup script
    Signed-off-by: Jesse Sanford <108698+jessesanford@users.noreply.github.com>

--- setup-ko-for-kubecon-demo.sh
+++ setup-ko-for-kubecon-demo.sh
@@ -8,4 +8,4 @@ echo "defaultBaseImage: ko.local/chainguard/static" > .ko.yaml
 
 export KO_DOCKER_REPO=gitea.cnoe.localtest.me:8443/giteaadmin/ && kustomize build config/overlays/local | ko resolve --local --insecure-registry -f - | yq .
 
-echo "Now run the following while offline 'export KO_DOCKER_REPO=gitea.cnoe.localtest.me:8443/giteaadmin/ && kustomize build config/overlays/local | ko resolve --local --insecure-registry -f - | kubectl apply -f'"
+echo "Now run the following while offline 'export KO_DOCKER_REPO=gitea.cnoe.localtest.me:8443/giteaadmin/ && kustomize build config/overlays/local | ko resolve --local --insecure-registry -f - | kubectl apply -f -'"
