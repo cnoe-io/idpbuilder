a90eabd2e992009cb85d4d9903d3fd641c8c2988
Jesse Sanford <108698+jessesanford@users.noreply.github.com>
2024-03-21 07:16:02 -0400

    better setup scripts
    Signed-off-by: Jesse Sanford <108698+jessesanford@users.noreply.github.com>

--- setup-second-demo-cluster-for-offline.sh
+++ setup-second-demo-cluster-for-offline.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-kind create cluster --name localdev --config kind-config.yaml
+kind create cluster --name secondlocaldev --config secondlocaldev-kind-config.yaml
 ./install-kuik.sh
-./idpbuilder create --package-dir examples/ref-implementation
+./idpbuilder create --build-name secondlocaldev --port 9443 --package-dir examples/ref-implementation
 read  -n 1 -p "Check argocd for installation success and then press any key to continue..."
 ./force-delete-ref-impl-argocd.sh
 ./delete-base-namespaces.sh
