52dcc81a2aaa1e8c12a06389b1a5748fdb344fe5
Jesse Sanford <108698+jessesanford@users.noreply.github.com>
2024-03-21 11:50:45 -0400

    longer timeouts
    Signed-off-by: Jesse Sanford <108698+jessesanford@users.noreply.github.com>

--- setup-demo-cluster-for-offline.sh
+++ setup-demo-cluster-for-offline.sh
@@ -3,10 +3,15 @@
 kind create cluster --name localdev --config kind-config.yaml
 echo "Installing Certmanager"
 kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.14.4/cert-manager.yaml
-kubectl -n cert-manager wait deployment/cert-manager-webhook --for condition=available
-kubectl -n cert-manager wait deployment/cert-manager --for condition=available
+kubectl -n cert-manager wait deployment/cert-manager-webhook --for condition=available --timeout=600s
+kubectl -n cert-manager wait deployment/cert-manager --for condition=available --timeout 600s
+sleep 10
 echo "Installing Kuik"
 ./install-kuik.sh
+kubectl -n kuik-system wait deployment/kube-image-keeper-controllers --for condition=available --timeout 600s
+kubectl -n kuik-system rollout status daemonset/kube-image-keeper-proxy --watch --timeout=600s
+kubectl -n kuik-system rollout status statefulset/kube-image-keeper-registry --watch --timeout=600s
+sleep 10
 echo "Running idpbuilder"
 ./idpbuilder create --package-dir examples/ref-implementation
 read  -n 1 -p "Check argocd for installation success and then press any key to continue..."
