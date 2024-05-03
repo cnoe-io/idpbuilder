bd62b428e8104c5d880bb696f7a49dada0dd804f
Nima Kaviani <17132353+nimakaviani@users.noreply.github.com>
2024-05-02 22:17:49 -0700

    Fix pull policy (#236)
    Signed-off-by: Nima Kaviani <nkaviani@amazon.com>

--- hack/gitea/generate-manifests.sh
+++ hack/gitea/generate-manifests.sh
@@ -5,13 +5,13 @@ INSTALL_YAML="pkg/controllers/localbuild/resources/gitea/k8s/install.yaml"
 GITEA_DIR="./hack/gitea"
 CHART_VERSION="9.5.1"
 
-echo "# GITEA INSTALL RESOURCES" > ${INSTALL_YAML}
-echo "# This file is auto-generated with 'hack/gitea/generate-manifests.sh'" >> ${INSTALL_YAML}
+echo "# GITEA INSTALL RESOURCES" >${INSTALL_YAML}
+echo "# This file is auto-generated with 'hack/gitea/generate-manifests.sh'" >>${INSTALL_YAML}
 
 helm repo add gitea-charts --force-update https://dl.gitea.com/charts/
 helm repo update
-helm template my-gitea gitea-charts/gitea -f ${GITEA_DIR}/values.yaml --version ${CHART_VERSION} >> ${INSTALL_YAML}
-sed -i '3d' ${INSTALL_YAML}
+helm template my-gitea gitea-charts/gitea -f ${GITEA_DIR}/values.yaml --version ${CHART_VERSION} >>${INSTALL_YAML}
+sed -i.bak '3d' ${INSTALL_YAML}
 
 # helm template for pvc uses Release.namespace which doesn't get set
 # when running the helm "template" command
@@ -19,6 +19,8 @@ sed -i '3d' ${INSTALL_YAML}
 # and: https://gitea.com/gitea/helm-chart/src/commit/3b2b700441e91a19a535e05de3a9eab2fef0b117/templates/gitea/pvc.yaml#L6
 # and: https://github.com/helm/helm/issues/3553#issuecomment-1186518158
 # and: https://github.com/splunk/splunk-connect-for-kubernetes/pull/790
-sed -i 's/namespace: default/namespace: gitea/g' ${INSTALL_YAML}
+sed -i.bak 's/namespace: default/namespace: gitea/g' ${INSTALL_YAML}
 
-cat ${GITEA_DIR}/ingress.yaml.tmpl >> ${INSTALL_YAML}
+cat ${GITEA_DIR}/ingress.yaml.tmpl >>${INSTALL_YAML}
+
+rm -rf "${INSTALL_YAML}.bak"
