33f73f20953b9e0e3b38891bf15d94576dd5db00
Manabu McCloskey <manabu.mccloskey@gmail.com>
2024-07-31 15:39:45 +0000

    cleanup commands
    Signed-off-by: Manabu McCloskey <manabu.mccloskey@gmail.com>

---
+++ pkg/cmd/get/clusters.go
@@ -0,0 +1,34 @@
+package get
+
+import (
+	"fmt"
+
+	"github.com/cnoe-io/idpbuilder/pkg/cmd/helpers"
+	"github.com/spf13/cobra"
+	"sigs.k8s.io/kind/pkg/cluster"
+)
+
+var ClustersCmd = &cobra.Command{
+	Use:     "clusters",
+	Short:   "Get idp clusters",
+	Long:    ``,
+	RunE:    list,
+	PreRunE: preClustersE,
+}
+
+func preClustersE(cmd *cobra.Command, args []string) error {
+	return helpers.SetLogger()
+}
+
+func list(cmd *cobra.Command, args []string) error {
+	provider := cluster.NewProvider(cluster.ProviderWithDocker())
+	clusters, err := provider.List()
+	if err != nil {
+		return fmt.Errorf("failed to list clusters: %w", err)
+	}
+
+	for _, c := range clusters {
+		fmt.Println(c)
+	}
+	return nil
+}
