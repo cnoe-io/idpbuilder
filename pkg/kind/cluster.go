385c649056d9fc1987ce05862dbec1807f090798
Manabu McCloskey <manabu.mccloskey@gmail.com>
2024-08-27 09:27:46 -0700

    ensure ingress port is available (#368)
    Signed-off-by: Manabu McCloskey <manabu.mccloskey@gmail.com>

--- pkg/kind/cluster.go
+++ pkg/kind/cluster.go
@@ -6,14 +6,17 @@ import (
 	"fmt"
 	"io/fs"
 	"os"
+	"strconv"
 	"strings"
 
 	"github.com/cnoe-io/idpbuilder/pkg/runtime"
 	"github.com/cnoe-io/idpbuilder/pkg/util"
 	"sigs.k8s.io/controller-runtime/pkg/log"
+	kindv1alpha4 "sigs.k8s.io/kind/pkg/apis/config/v1alpha4"
 	"sigs.k8s.io/kind/pkg/cluster"
 	"sigs.k8s.io/kind/pkg/cluster/nodes"
 	"sigs.k8s.io/kind/pkg/cluster/nodeutils"
+	"sigs.k8s.io/yaml"
 )
 
 var (
@@ -66,7 +69,7 @@ func (c *Cluster) getConfig() ([]byte, error) {
 	}
 
 	if err != nil {
-		return []byte{}, err
+		return nil, fmt.Errorf("reading kind config: %w", err)
 	}
 
 	var portMappingPairs []PortMapping
@@ -82,8 +85,6 @@ func (c *Cluster) getConfig() ([]byte, error) {
 				portMappingPairs[i] = PortMapping{parts[0], parts[1]}
 			}
 		}
-	} else {
-		portMappingPairs = nil
 	}
 
 	var retBuff []byte
@@ -92,7 +93,20 @@ func (c *Cluster) getConfig() ([]byte, error) {
 		KubernetesVersion:         c.kubeVersion,
 		ExtraPortsMapping:         portMappingPairs,
 	}); err != nil {
-		return []byte{}, err
+		return nil, err
+	}
+
+	if c.kindConfigPath != "" {
+		parsedCluster, err := c.ensureCorrectConfig(retBuff)
+		if err != nil {
+			return nil, fmt.Errorf("ensuring custom kind config is correct: %w", err)
+		}
+
+		out, err := yaml.Marshal(parsedCluster)
+		if err != nil {
+			return nil, fmt.Errorf("marshaling custom kind cluster config: %w", err)
+		}
+		return out, nil
 	}
 
 	return retBuff, nil
@@ -213,4 +227,43 @@ func (c *Cluster) Reconcile(ctx context.Context, recreate bool) error {
 
 func (c *Cluster) ExportKubeConfig(name string, internal bool) error {
 	return c.provider.ExportKubeConfig(name, c.kubeConfigPath, internal)
+}
+
+func (c *Cluster) ensureCorrectConfig(in []byte) (kindv1alpha4.Cluster, error) {
+	// see pkg/kind/resources/kind.yaml.tmpl and pkg/controllers/localbuild/resources/nginx/k8s/ingress-nginx.yaml
+	// defines which container port we should be looking for.
+	containerPort := "443"
+	if c.cfg.Protocol == "http" {
+		containerPort = "80"
+	}
+	parsedCluster := kindv1alpha4.Cluster{}
+	err := yaml.Unmarshal(in, &parsedCluster)
+	if err != nil {
+		return kindv1alpha4.Cluster{}, fmt.Errorf("parsing kind config: %w", err)
+	}
+
+	appendNecessaryPort := true
+nodes:
+	for i := range parsedCluster.Nodes {
+		node := parsedCluster.Nodes[i]
+		for _, pm := range node.ExtraPortMappings {
+			if strconv.Itoa(int(pm.HostPort)) == c.cfg.Port {
+				appendNecessaryPort = false
+				break nodes
+			}
+		}
+	}
+
+	if appendNecessaryPort && len(parsedCluster.Nodes) != 0 {
+		hp, err := strconv.Atoi(c.cfg.Port)
+		if err != nil {
+			return kindv1alpha4.Cluster{}, fmt.Errorf("converting port, %s, to int: %w", c.cfg.Port, err)
+		}
+		// either "80" or "443". No need to check for err
+		cp, _ := strconv.Atoi(containerPort)
+
+		parsedCluster.Nodes[0].ExtraPortMappings = append(parsedCluster.Nodes[0].ExtraPortMappings, kindv1alpha4.PortMapping{ContainerPort: int32(cp), HostPort: int32(hp)})
+	}
+
+	return parsedCluster, nil
 }
