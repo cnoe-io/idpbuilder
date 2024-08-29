fba981a3d4784d4a6ea31844e63c3ed530040467
Manabu McCloskey <manabu.mccloskey@gmail.com>
2024-08-29 11:42:02 -0700

    Change logging format (#370)
    Signed-off-by: Manabu McCloskey <manabu.mccloskey@gmail.com>

--- pkg/cmd/root.go
+++ pkg/cmd/root.go
@@ -20,6 +20,7 @@ var rootCmd = &cobra.Command{
 
 func init() {
 	rootCmd.PersistentFlags().StringVarP(&helpers.LogLevel, "log-level", "l", "info", helpers.LogLevelMsg)
+	rootCmd.PersistentFlags().BoolVar(&helpers.ColoredOutput, "color", false, helpers.ColoredOutputMsg)
 	rootCmd.AddCommand(create.CreateCmd)
 	rootCmd.AddCommand(get.GetCmd)
 	rootCmd.AddCommand(delete.DeleteCmd)
