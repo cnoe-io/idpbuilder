a7e8bd9b33d77fbc7a622685419d80cfdf417ca1
Manabu McCloskey <manabu.mccloskey@gmail.com>
2024-08-29 18:07:56 +0000

    remove C flag
    Signed-off-by: Manabu McCloskey <manabu.mccloskey@gmail.com>

--- pkg/cmd/root.go
+++ pkg/cmd/root.go
@@ -20,7 +20,7 @@ var rootCmd = &cobra.Command{
 
 func init() {
 	rootCmd.PersistentFlags().StringVarP(&helpers.LogLevel, "log-level", "l", "info", helpers.LogLevelMsg)
-	rootCmd.PersistentFlags().BoolVarP(&helpers.ColoredOutput, "color", "C", false, helpers.ColoredOutputMsg)
+	rootCmd.PersistentFlags().BoolVar(&helpers.ColoredOutput, "color", false, helpers.ColoredOutputMsg)
 	rootCmd.AddCommand(create.CreateCmd)
 	rootCmd.AddCommand(get.GetCmd)
 	rootCmd.AddCommand(delete.DeleteCmd)
