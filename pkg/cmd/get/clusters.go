0207f16454be8d60ed779adee031ba681395fa1a
cmoulliard <cmoulliard@redhat.com>
2024-11-14 16:04:17 +0100

    Populate a list of kuybe clients; one per cluster/context
    Signed-off-by: cmoulliard <cmoulliard@redhat.com>

--- pkg/cmd/get/clusters.go
+++ pkg/cmd/get/clusters.go
@@ -10,12 +10,19 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/tools/clientcmd/api"
 	"os"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/kind/pkg/cluster"
+	"slices"
 	"strings"
 )
+
+// ClusterManager holds the clients for the different idpbuilder clusters
+type ClusterManager struct {
+	clients map[string]client.Client // map of cluster name to client
+}
 
 var ClustersCmd = &cobra.Command{
 	Use:     "clusters",
@@ -50,9 +57,16 @@ func list(cmd *cobra.Command, args []string) error {
 		os.Exit(1)
 	}
 
-	cli, err := helpers.GetKubeClient(kubeConfig)
+	// TODO: Check if we need it or not like also if the new code handle the kubeconfig path passed as parameter
+	_, err = helpers.GetKubeClient(kubeConfig)
 	if err != nil {
 		logger.Error(err, "failed to create the kube client.")
+		os.Exit(1)
+	}
+
+	config, err := helpers.LoadKubeConfig()
+	if err != nil {
+		logger.Error(err, "failed to load the kube config.")
 		os.Exit(1)
 	}
 
@@ -63,13 +77,12 @@ func list(cmd *cobra.Command, args []string) error {
 		logger.Error(err, "failed to list clusters.")
 	}
 
+	// Populate a list of Kube client for each cluster/context matching a idpbuilder cluster
+	manager, _ := CreateKubeClientForEachIDPCluster(config, clusters)
+
+	fmt.Printf("\n")
 	for _, cluster := range clusters {
 		fmt.Printf("Cluster: %s\n", cluster)
-
-		config, err := helpers.LoadKubeConfig()
-		if err != nil {
-			logger.Error(err, "failed to load the kube config.")
-		}
 
 		// Search about the idp cluster within the kubeconfig file and show information
 		c, found := findClusterByName(config, "kind-"+cluster)
@@ -78,53 +91,56 @@ func list(cmd *cobra.Command, args []string) error {
 		} else {
 			fmt.Printf("URL of the kube API server: %s\n", c.Server)
 			fmt.Printf("TLS Verify: %t\n", c.InsecureSkipTLSVerify)
-		}
 
-		fmt.Println("----------------------------------------")
-	}
+			cli, err := GetClientForCluster(manager, cluster)
+			if err != nil {
+				logger.Error(err, "failed to get the cluster/context for the cluster: %s.", cluster)
+			}
+			// Print the external port that users can access using the ingress nginx proxy
+			service := corev1.Service{}
+			namespacedName := types.NamespacedName{
+				Name:      "ingress-nginx-controller",
+				Namespace: "ingress-nginx",
+			}
+			err = cli.Get(context.TODO(), namespacedName, &service)
+			if err != nil {
+				logger.Error(err, "failed to get the ingress service on the cluster.")
+			}
+			fmt.Printf("External Port: %d", findExternalHTTPSPort(service))
 
-	// Print the external port that users can access using the ingress nginx proxy
-	service := corev1.Service{}
-	namespacedName := types.NamespacedName{
-		Name:      "ingress-nginx-controller",
-		Namespace: "ingress-nginx",
-	}
-	err = cli.Get(context.TODO(), namespacedName, &service)
-	if err != nil {
-		logger.Error(err, "failed to get the ingress service on the cluster.")
-	}
-	fmt.Printf("External Port: %d\n", findExternalHTTPSPort(service))
+			// Let's check what the current node reports
+			var nodeList corev1.NodeList
+			err = cli.List(context.TODO(), &nodeList)
+			if err != nil {
+				logger.Error(err, "failed to list nodes for the current kube cluster.")
+			}
 
-	// Let's check what the current node reports
-	var nodeList corev1.NodeList
-	err = cli.List(context.TODO(), &nodeList)
-	if err != nil {
-		logger.Error(err, "failed to list nodes for the current kube cluster.")
-	}
+			for _, node := range nodeList.Items {
+				nodeName := node.Name
+				fmt.Printf("\n\n")
+				fmt.Printf("Node: %s\n", nodeName)
 
-	for _, node := range nodeList.Items {
-		nodeName := node.Name
-		fmt.Printf("Node: %s\n", nodeName)
+				for _, addr := range node.Status.Addresses {
+					switch addr.Type {
+					case corev1.NodeInternalIP:
+						fmt.Printf("Internal IP: %s\n", addr.Address)
+					case corev1.NodeExternalIP:
+						fmt.Printf("External IP: %s\n", addr.Address)
+					}
+				}
 
-		for _, addr := range node.Status.Addresses {
-			switch addr.Type {
-			case corev1.NodeInternalIP:
-				fmt.Printf("Internal IP: %s\n", addr.Address)
-			case corev1.NodeExternalIP:
-				fmt.Printf("External IP: %s\n", addr.Address)
+				// Show node capacity
+				fmt.Printf("Capacity of the node: \n")
+				printFormattedResourceList(node.Status.Capacity)
+
+				// Show node allocated resources
+				err = printAllocatedResources(context.Background(), cli, node.Name)
+				if err != nil {
+					logger.Error(err, "Failed to get the node's allocated resources.")
+				}
 			}
 		}
-
-		// Show node capacity
-		fmt.Printf("Capacity of the node: \n")
-		printFormattedResourceList(node.Status.Capacity)
-
-		// Show node allocated resources
-		err = printAllocatedResources(context.Background(), cli, node.Name)
-		if err != nil {
-			logger.Error(err, "Failed to get the node's allocated resources.")
-		}
-		fmt.Println("--------------------")
+		fmt.Println("----------------------------------------")
 	}
 
 	return nil
@@ -191,4 +207,43 @@ func findExternalHTTPSPort(service corev1.Service) int32 {
 		}
 	}
 	return targetPort.Port
+}
+
+// GetClientForCluster returns the client for the specified cluster/context name
+func GetClientForCluster(m *ClusterManager, clusterName string) (client.Client, error) {
+	cl, exists := m.clients["kind-"+clusterName]
+	if !exists {
+		return nil, fmt.Errorf("no client found for cluster %q", clusterName)
+	}
+	return cl, nil
+}
+
+func CreateKubeClientForEachIDPCluster(config *api.Config, clusterList []string) (*ClusterManager, error) {
+	// Initialize the ClusterManager with a map of kube Client
+	manager := &ClusterManager{
+		clients: make(map[string]client.Client),
+	}
+
+	for contextName := range config.Contexts {
+		// Check if the kubconfig contains the cluster name
+		// We remove the prefix "kind-" to find the cluster name from the slice
+		if slices.Contains(clusterList, contextName[5:]) {
+			cfg, err := clientcmd.NewNonInteractiveClientConfig(*config, contextName, &clientcmd.ConfigOverrides{}, nil).ClientConfig()
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Failed to build client for context %q: %v\n", contextName, err)
+				continue
+			}
+
+			cl, err := client.New(cfg, client.Options{})
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Failed to create client for context %q: %v\n", contextName, err)
+				continue
+			}
+
+			manager.clients[contextName] = cl
+			// fmt.Printf("Client created for context %q\n", contextName)
+		}
+
+	}
+	return manager, nil
 }
