98b72345c12a2e7201f6781a93d1d334f3a7277c
Manabu McCloskey <manabu.mccloskey@gmail.com>
2024-11-25 09:10:15 -0800

    add labels to applications (#461)
    Signed-off-by: Manabu McCloskey <manabu.mccloskey@gmail.com>

--- pkg/util/util_test.go
+++ pkg/util/util_test.go
@@ -3,6 +3,12 @@ package util
 import (
 	"strconv"
 	"testing"
+
+	"github.com/cnoe-io/idpbuilder/api/v1alpha1"
+	"github.com/stretchr/testify/assert"
+	"k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 )
 
 var specialCharMap = make(map[string]struct{})
@@ -40,5 +46,91 @@ func TestGeneratePassword(t *testing.T) {
 		if counts[2] < numDigits {
 			t.Fatalf("min number of digits not generated")
 		}
+	}
+}
+
+type MockObject struct {
+	v1.ObjectMeta
+}
+
+func (m *MockObject) GetObjectKind() schema.ObjectKind {
+	return nil
+}
+
+func (m *MockObject) DeepCopyObject() runtime.Object {
+	return nil
+}
+
+func TestSetPackageLabels(t *testing.T) {
+	testCases := []struct {
+		name           string
+		objectName     string
+		initialLabels  map[string]string
+		expectedLabels map[string]string
+	}{
+		{
+			name:          "No initial labels",
+			objectName:    "test-package",
+			initialLabels: nil,
+			expectedLabels: map[string]string{
+				v1alpha1.PackageNameLabelKey: "test-package",
+				v1alpha1.PackageTypeLabelKey: v1alpha1.PackageTypeLabelCustom,
+			},
+		},
+		{
+			name:       "With initial labels",
+			objectName: "test-package-one",
+			initialLabels: map[string]string{
+				"existing":                   "label",
+				v1alpha1.PackageNameLabelKey: "incorrect",
+			},
+			expectedLabels: map[string]string{
+				"existing":                   "label",
+				v1alpha1.PackageNameLabelKey: "test-package-one",
+				v1alpha1.PackageTypeLabelKey: v1alpha1.PackageTypeLabelCustom,
+			},
+		},
+		{
+			name:          "ArgoCD package",
+			objectName:    v1alpha1.ArgoCDPackageName,
+			initialLabels: nil,
+			expectedLabels: map[string]string{
+				v1alpha1.PackageNameLabelKey: v1alpha1.ArgoCDPackageName,
+				v1alpha1.PackageTypeLabelKey: v1alpha1.PackageTypeLabelCore,
+			},
+		},
+		{
+			name:          "Gitea package",
+			objectName:    v1alpha1.GiteaPackageName,
+			initialLabels: nil,
+			expectedLabels: map[string]string{
+				v1alpha1.PackageNameLabelKey: v1alpha1.GiteaPackageName,
+				v1alpha1.PackageTypeLabelKey: v1alpha1.PackageTypeLabelCore,
+			},
+		},
+		{
+			name:          "IngressNginx package",
+			objectName:    v1alpha1.IngressNginxPackageName,
+			initialLabels: nil,
+			expectedLabels: map[string]string{
+				v1alpha1.PackageNameLabelKey: v1alpha1.IngressNginxPackageName,
+				v1alpha1.PackageTypeLabelKey: v1alpha1.PackageTypeLabelCore,
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			obj := &MockObject{
+				ObjectMeta: v1.ObjectMeta{
+					Name:   tc.objectName,
+					Labels: tc.initialLabels,
+				},
+			}
+
+			SetPackageLabels(obj)
+
+			assert.Equal(t, tc.expectedLabels, obj.GetLabels())
+		})
 	}
 }
