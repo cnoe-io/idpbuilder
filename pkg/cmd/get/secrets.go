a87a5cd84bb49e6e902d9dc4cdb0dd42fa0beb26
cmoulliard <cmoulliard@redhat.com>
2025-01-06 10:31:42 +0100

    Remove non used packages
    Signed-off-by: cmoulliard <cmoulliard@redhat.com>

--- pkg/cmd/get/secrets.go
+++ pkg/cmd/get/secrets.go
@@ -6,21 +6,18 @@ import (
 	"github.com/cnoe-io/idpbuilder/api/v1alpha1"
 	"github.com/cnoe-io/idpbuilder/pkg/build"
 	"github.com/cnoe-io/idpbuilder/pkg/k8s"
-	"github.com/cnoe-io/idpbuilder/pkg/entity"  
 	"github.com/cnoe-io/idpbuilder/pkg/printer"
 	"github.com/cnoe-io/idpbuilder/pkg/types"
 	"github.com/spf13/cobra"
 	"io"
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"  
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/selection"
 	"k8s.io/client-go/util/homedir"
 	"os"
 	"path/filepath"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-  "strings"
 )
 
 const (
