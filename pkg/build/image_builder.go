d43e949b712d45415ab043bb6c77d2622c29d4fd
Jesse Sanford <108698+jessesanford@users.noreply.github.com>
2025-09-15 04:59:47 +0000

    fix: remove feature flag blocking build command

--- pkg/build/image_builder.go
+++ pkg/build/image_builder.go
@@ -38,10 +38,6 @@ func NewBuilder(storageDir string) (*Builder, error) {
 
 // BuildImage builds an OCI image from a directory context
 func (b *Builder) BuildImage(ctx context.Context, opts BuildOptions) (*BuildResult, error) {
-	// Check feature flag
-	if !IsImageBuilderEnabled() {
-		return nil, ErrFeatureDisabled
-	}
 
 	// Validate options
 	if opts.ContextPath == "" {
